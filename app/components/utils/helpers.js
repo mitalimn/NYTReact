b44e3c38f503c651cf24fcfe89701a2d24e633ab
Mitali Naik <mitalinaik@yahoo.com>
2017-03-03 23:48:20 -0600

    API delete

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -77,4 +77,67 @@ var apiSave = function(articleObj){
   
 }
 
+// API Post Request Function
+var apiGet = function(){
+
+  // Get API Post URL (this allows it to work in both localhost and heroku)
+  var apiURL = window.location.origin + '/api/saved';
+
+  // Create a JavaScript *Promise*
+  return new Promise(function (fulfill, reject){
+
+    // Re-format the article Object to match the Mongo Model (ie we need to take off the the id)
+    axios.get(apiURL).then(function(response) {
+
+      // Error handling / fullfil promise if successful query
+      if(response){
+        fulfill(response);
+      }
+      else{
+        reject("");
+      }
+
+    });
+    
+  });
+  
+}
+
+// API Post Request Function
+var apiDelete = function(deleteArticleId){
+
+  // Get API Post URL (this allows it to work in both localhost and heroku)
+  var apiURL = window.location.origin + '/api/delete/' + deleteArticleId;
+
+  // Create a JavaScript *Promise*
+  return new Promise(function (fulfill, reject){
+
+    // Send the MongoDB Id for deletion
+    axios.post(apiURL).then(function(response) {
+
+      // Error handling / fullfil promise if successful query
+      if(response){
+        fulfill(response);
+      }
+      else{
+        reject("");
+      }
+
+    });
+
+  });
+
+}
+
+
+
+
+
+// Export all helper functions
+module.exports = {
+ articleQuery,
+ apiSave,
+ apiGet,
+ apiDelete
+}
 
